79d8a162d8840e6fef9a7fec1a8374146f131046
Alex Levankou <alexlevankou@gmail.com>
2019-01-15 23:09:13 +0300

    Initial commit

---
+++ app/src/main/java/by/alexlevankou/testapp/view/MainActivity.java
@@ -0,0 +1,51 @@
+package by.alexlevankou.testapp.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(by.alexlevankou.testapp.R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(by.alexlevankou.testapp.R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(by.alexlevankou.testapp.R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(by.alexlevankou.testapp.R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == by.alexlevankou.testapp.R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ gradlew.bat
@@ -0,0 +1,45 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="3" />
+      <entry action="$Delete" count="48" />
+      <entry action="$Paste" count="44" />
+      <entry action="$Redo" count="1" />
+      <entry action="$SelectAll" count="16" />
+      <entry action="$Undo" count="582" />
+      <entry action="About" count="2" />
+      <entry action="Android Lint Quick Fixes" count="2" />
+      <entry action="Android.CreateResourceDirectory" count="1" />
+      <entry action="Android.LinkExternalCppProject" count="1" />
+      <entry action="Android.RunAndroidAvdManager" count="20" />
+      <entry action="Android.RunAndroidSdkManager" count="10" />
+      <entry action="Android.SyncProject" count="295" />
+      <entry action="AndroidConnectDebuggerAction" count="1" />
+      <entry action="Back" count="50" />
+      <entry action="ChangesView.DeleteUnversioned" count="2" />
+      <entry action="ChangesView.DeleteUnversioned.From.Dialog" count="2" />
+      <entry action="ChangesView.Ignore" count="1" />
+      <entry action="ChangesView.Refresh" count="6" />
+      <entry action="ChangesView.Revert" count="26" />
+      <entry action="CheckinFiles" count="2" />
+      <entry action="CheckinProject" count="70" />
+      <entry action="ChooseDebugConfiguration" count="10" />
+      <entry action="ChooseRunConfiguration" count="2" />
+      <entry action="CleanGradleProject" count="10" />
+      <entry action="CloseProject" count="2" />
+      <entry action="CollapseSelection" count="9" />
+      <entry action="CommentByBlockComment" count="2" />
+      <entry action="CommentByLineComment" count="73" />
+      <entry action="CompareTwoFiles" count="1" />
+      <entry action="CopyPaths" count="1" />
+      <entry action="Cvs.Import" count="4" />
+      <entry action="Debug" count="568" />
+      <entry action="Debugger.EditFrameSource" count="20" />
+      <entry action="DialogOkAction Enter" count="2" />
+      <entry action="Diff.ShowDiff" count="19" />
+      <entry action="EditSource" count="184" />
+      <entry action="EditorBackSpace" count="7922" />
+      <entry action="EditorChooseLookupItem" count="1296" />
+      <entry action="EditorCopy" count="1301" />
+      <entry action="EditorCut" count="203" />
+     
