2ac22abc07d4dd8dca3f900ae297882bb162009e
Alex Levankou <alexlevankou@gmail.com>
2019-01-16 23:26:31 +0300

    Layout fix.

--- app/src/main/java/by/alexlevankou/testapp/view/MainActivity.java
+++ app/src/main/java/by/alexlevankou/testapp/view/MainActivity.java
@@ -2,13 +2,13 @@ package by.alexlevankou.testapp.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import by.alexlevankou.testapp.R;
 import by.alexlevankou.testapp.adapter.RecyclerViewAdapter;
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements BaseContract.View
 
         recyclerView = findViewById(R.id.list);
         adapter = new RecyclerViewAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
